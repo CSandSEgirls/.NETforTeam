e7ebd63b63563ab88624b97a1ee33e908e6b3854
Nazym Turysbek <nazym_turysbekova@mail.ru>
2018-02-06 21:14:47 +0600

    employees added

---
+++ kursovayaOne/Models/Employees.cs
@@ -0,0 +1,19 @@
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace kursovayaOne.Models{
+    public class Employees
+    {
+        private int id { get; set; }
+
+        private string name { get; set; }
+
+        private string job_position { get; set; }
+
+        private string email {get; set;}
+
+    }
+}
--- kursovayaOne/Program.cs
+++ kursovayaOne/Program.cs
@@ -7,18 +7,23 @@ namespace kursovayaOne
     { 
         
         static readonly string readerPath = "App_Data/reader.csv";  
-        static readonly string recordPath = "App_Data/records.csv";  
+        static readonly string recordPath = "App_Data/records.csv"; 
+        static readonly string empoyeesPath = "App_Data/employees.csv";   
 
         static void Main(string[] args)
         {
              var readerStore  = new ReaderStore() { Path =  readerPath };
-             var readerList  = readerStore.GetCollection();
+             var readerList   = readerStore.GetCollection();
 
             var recordStore  = new RecordStore() { Path =  recordPath };
-            var recordList  = recordStore.GetCollection();
+            var recordList   = recordStore.GetCollection();
 
+            var employeeStore  = new EmployeesStore() { Path = empoyeesPath };
+            var employeeList     = employeeStore.GetCollection();
 
-             var readerss = readerList;
+            var readerss = readerList;
+            var empllist = employeeList;  
+            
              
             var prodTemplate = "{0} | {1} | {2} ";
 
@@ -33,25 +38,39 @@ namespace kursovayaOne
                         p.Name,
                         p.Course));
             }
+              var prodTemplate = "{0} | {1} | {2} | {3}";
 
-            var records = recordList;
-            var prodTemplate = "{0} | {1} | {2} ";
+            // Console.WriteLine(string.Format(prodTemplate, 
+            // "Id", 
+            // "Name", 
+            // "job_position",
+            // "email"));
 
-            Console.WriteLine(string.Format(prodTemplate, 
-            "borrowed_date", 
-            "returned_date", 
-            "reader_id", 
-            "employee_id", 
-            "book_id"));
+            // foreach(var p in empllist ){
+            //     Console.WriteLine(string.Format(prodTemplate,
+            //             p.employee_id, 
+            //             p.name,
+            //             p.job_position,
+            //             p.email));
+            // }
+            // var records = recordList;
+            // var prodTemplate = "{0} | {1} | {2} ";
 
-            foreach(var record in records){
-                Console.WriteLine(string.Format(prodTemplate,
-                        p.borrowed_date, 
-                        p.returned_date,
-                        p.reader_id, 
-                        p.employee_id,
-                        p.book_id));
-            }
+            // Console.WriteLine(string.Format(prodTemplate, 
+            // "borrowed_date", 
+            // "returned_date", 
+            // "reader_id", 
+            // "employee_id", 
+            // "book_id"));
+
+            // foreach(var record in records){
+            //     Console.WriteLine(string.Format(prodTemplate,
+            //             p.borrowed_date, 
+            //             p.returned_date,
+            //             p.reader_id, 
+            //             p.employee_id,
+            //             p.book_id));
+            // }
         }
     }
 }
